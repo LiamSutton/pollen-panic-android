fee96b6a1e99bbf9378a403c7ba7339b4ab913d9
Liam Sutton <liamsutton1997@outlook.com>
2020-12-04 14:15:44 +0000

    First Commit

---
+++ app/src/main/java/com/ls/pollenpanic/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ls.pollenpanic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
