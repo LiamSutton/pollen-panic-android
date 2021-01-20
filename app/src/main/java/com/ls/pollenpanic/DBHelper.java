dd964279b46dbfadc89df18bcbce03462c90ffb3
Liam Sutton <liamsutton1997@outlook.com>
2021-01-20 12:49:39 +0000

    DB Helper commented

--- app/src/main/java/com/ls/pollenpanic/DBHelper.java
+++ app/src/main/java/com/ls/pollenpanic/DBHelper.java
@@ -9,32 +9,41 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+/**
+ * This class is responsible for facilitating communication between the application and our database
+ */
 public class DBHelper extends SQLiteOpenHelper {
-    private Context context;
+    private Context context; // current context
 
-    private static final String DATABASE_NAME = "PollenPanicLeaderBoard.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "PollenPanicLeaderBoard.db"; // the database to connect to
+    private static final int DATABASE_VERSION = 1; // the current version of our database
 
-    private static final String TABLE_NAME = "leader_board";
-    private static final String COLUMN_ID = "_id";
-    private static final String COLUMN_USER_NAME = "user_name";
-    private static final String COLUMN_SCORE = "score";
+    private static final String TABLE_NAME = "leader_board"; // the table inside our database
+    private static final String COLUMN_ID = "_id"; // column for id
+    private static final String COLUMN_USER_NAME = "user_name"; // column for user name
+    private static final String COLUMN_SCORE = "score"; // column for score
 
+    // Constructor
     public DBHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
 
+    // Creates the required tables for our application
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createDB = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s INTEGER);", TABLE_NAME, COLUMN_ID, COLUMN_USER_NAME, COLUMN_SCORE);
         db.execSQL(createDB);
     }
 
+    // Drops the current information and re-creates the required tables
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         String dropDB = String.format("DROP TABLE IF EXISTS %s;", TABLE_NAME);
+        onCreate(db);
     }
+
+    // When a user submits their score to the leaderboard, their name and score are stored in the table.
     public void addNewScore(String userName, int score) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -50,6 +59,7 @@ public class DBHelper extends SQLiteOpenHelper {
         }
     }
 
+    // Grabs the top n hi-scores to display on the leaderboard
     public Cursor getTopHighScores(int n) {
         String query = String.format("SELECT * FROM %s ORDER BY %s DESC LIMIT 0,%d", TABLE_NAME, COLUMN_SCORE, n);
 
