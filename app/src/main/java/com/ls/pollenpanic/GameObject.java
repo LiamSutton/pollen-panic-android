ee7833b28d9b062ec390ff491ec37a73372e76e2
Liam Sutton <liamsutton1997@outlook.com>
2021-01-20 13:13:20 +0000

    GameObject documented

--- app/src/main/java/com/ls/pollenpanic/GameObject.java
+++ app/src/main/java/com/ls/pollenpanic/GameObject.java
@@ -2,17 +2,19 @@ package com.ls.pollenpanic;
 
 import android.graphics.drawable.Drawable;
 
+// The base class from which all GameObjects derive
 public class GameObject {
 
-    protected float xPosition;
-    protected float yPosition;
-    protected float xDirection;
-    protected float yDirection;
-    protected int width;
-    protected int height;
+    protected float xPosition; // current xPosition
+    protected float yPosition; // current yPosition
+    protected float xDirection; // current xDirection
+    protected float yDirection; // current yDirection
+    protected int width; // width of the GameObjects sprite
+    protected int height; // height of the GameObjects sprite
 
-    Drawable sprite;
+    Drawable sprite; // used to store an image representing the GameObject
 
+    // Constructor
     public GameObject(float xPosition, float yPosition, float xDirection, float yDirection, Drawable sprite) {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
